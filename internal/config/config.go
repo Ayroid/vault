e6782e71099550620455b70fbaea62119df96cc5
Ayroid <ayushsk0000@gmail.com>
2026-02-02 13:44:58 +0530

    update: banner

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -10,3 +11,20 @@ var (
 	TSXVaultPath = filepath.Join(HomeDir, ".vault", "components", "tsx")
 	JSXVaultPath = filepath.Join(HomeDir, ".vault", "components", "jsx")
 )
+
+type Config struct {
+	GitTrackingEnabled bool   `json:"git_tracking_enabled"`
+	GitInitialized     bool   `json:"git_initialized"`
+	AutoSync           bool   `json:"auto_sync"`
+	RemoteUrl          string `json:"remote_url"`
+	FirstRunCompleted  bool   `json:"first_run_completed"`
+}
+
+func Load() error {
+	config, err := os.ReadFile("~/.vault/config.json")
+	if err != nil {
+		return err
+	}
+	fmt.Println(config)
+	return nil
+}
